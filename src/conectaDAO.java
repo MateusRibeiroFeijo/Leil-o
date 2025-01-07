8fcc5f906cc67a86c68c11dc72a4bc3ddb8bcde5
Mateus Ribeiro <mateusrfeijo@gmail.com>
2025-01-07 19:54:45 -0300

    Ajustes na conexão, em conectaDAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -12,7 +12,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/Casa_Leiloes_UC11","root", "matti170507");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
